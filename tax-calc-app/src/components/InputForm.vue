f221e38a1284c89d44ade9ed10292510ddc74c53
patrycja-io <patrycjatyra@protonmail.com>
2020-11-25 17:03:31 +0000

    added script to input

--- tax-calc-app/src/components/InputForm.vue
+++ tax-calc-app/src/components/InputForm.vue
@@ -26,9 +26,46 @@
 </template>
 
 <script>
+import Input from "./Input";
 export default {
-  name: 'InputForm'
-}
+  name: "InputForm",
+  components: {
+    Input
+  },
+  data() {
+    return {
+      inputs: {
+        incomeValue: "",
+        year: "",
+      },
+        year: [{ label: "2018", value: "2018" }],
+      }
+    }
+  }
+ computed: {
+    isEnabled: function() {
+      return !!Object.values(this.inputs).every(Boolean);
+    }
+  }
+  methods: {
+    input: function(input) {
+      if (input.type === "input") {
+        this.incomeValue = input.value;
+      }
+    }
+    handleSubmit: function() {
+      const { isInChurch, stateOfResidence, year } = this.inputs;
+      const inputValues = {
+        ...this.inputs,
+        year: year.value,
+        isInChurch: isInChurch.value,
+        stateOfResidence: stateOfResidence.value
+      };
+      this.$emit("submitted", inputValues);
+    }
+  }
+};
+
 </script>
 
 <style lang="scss" src="../assets/styles/InputForm.scss"/>
