e1a27f0498ba7f115e129d816d561c1055d1aff2
patrycja-io <patrycjatyra@protonmail.com>
2020-11-25 16:59:27 +0000

    added input fields

--- tax-calc-app/src/components/InputForm.vue
+++ tax-calc-app/src/components/InputForm.vue
@@ -1,5 +1,28 @@
 <template>
+  <form @submit.prevent="handleSubmit">
+    <Input
+      type="input"
+      label="Gross Salary"
+      validation="required|numeric"
+      v-model="inputs.incomeValue"
+      @input="input"
+    />
+    <Input
+      type="dropdown"
+      label="Year"
+      validation="required"
+      :options="options.year"
+      v-model="inputs.year"
+    />
 
+    <button
+      class='submit-btn'
+      :disabled="!isEnabled"
+      type="submit"
+    >
+      Calculate!
+    </button>
+  </form>
 </template>
 
 <script>
