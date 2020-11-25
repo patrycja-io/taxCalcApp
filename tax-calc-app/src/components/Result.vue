b0ed5a481a8b0243a13b5773c6d0d88b518eb730
patrycja-io <patrycjatyra@protonmail.com>
2020-11-25 17:06:43 +0000

    added result template

---
+++ tax-calc-app/src/components/Result.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <table>
+      <tr class="table-head">
+        <th class="name">Name</th>
+        <th class="value">Value</th>
+      </tr>
+      <tr
+        v-for='element in results'
+        :key="element.label"
+      >
+        <td>{{element.label}}</td>
+        <td class="value">
+          {{element.value}}
+        </td>
+      </tr>
+    </table>
+
+    <button
+      v-on:click="handleBackClick"
+      class="btn-back"
+    >
+      Back
+    </button>
+  </div>
+</template>
